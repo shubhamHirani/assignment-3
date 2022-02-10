1f8d0258c2f75e62a4759aa339d267bb7d915854
Mtech <shubham.hirani@marutitech.com>
2022-02-10 18:17:52 +0530

    working 2nd

--- data-validator/router/data-validator.js
+++ data-validator/router/data-validator.js
@@ -24,7 +24,7 @@ router.get('/consumer', auth, async(req,res)=>{
         console.log("message: ", input)
         if(input.random%10 ===0){
             setTimeout(()=>{
-                input.random = getRandom(60)
+                input.random =getRandom(60)
                 input.category='retired'
                 console.log("message: ", input)
                 if(input.random%10 ===0){
@@ -40,41 +40,33 @@ router.get('/consumer', auth, async(req,res)=>{
                 }
                 const url = 'http://127.0.0.1:3002/add/message'
                 
-                const request =axios.post(url,data,config).then((response)=>console.log(response.data))
+                const request =axios.post(url,data,config)
         
                         console.log("message: ", input)
                     },4000)
                 }
+                else{
 
                 const data = [{message:input.message,category: input.category ,user_id: input.user_id, created_time : new Date()}]
                 const config = {
                     headers: { 'Authorization' : `Bearer ${req.token}`, CorelationId : uuidv4()}
+                
                 }
                 const url = 'http://127.0.0.1:3002/add/message'
                 
-                const request =axios.post(url,data,config).then((response)=>console.log(response.data))
+                const request =axios.post(url,data,config)
+            }
             },4000)
         }
+        else{
         const data = [{message:input.message,category: input.category ,user_id: input.user_id, created_time : new Date()}]
         const config = {
             headers: { 'Authorization' : `Bearer ${req.token}`, "CorelationId" : uuidv4()}
         }
         const url = 'http://127.0.0.1:3002/add/message'
         
-        const request =axios.post(url,data,config).then((response)=>console.log(response.data))
-        // request.end()
-        // .then(response=>console.log(response.data), 
-        // error=>{
-        //     console.log(error)
-        // }).catch('error')
-        // .then(response=>console.log(response))
-        //     .then(function (response) {
-        //         console.log(response);
-        //       })
-        //       .catch(function (error) {
-        //         console.log(error);
-        //       })
-        //       console.log('1');
+        const request =axios.post(url,data,config)
+    }
 }, { noAck : true})
 
 })
